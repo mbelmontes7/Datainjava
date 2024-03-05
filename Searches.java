3b576916fb36e32b1e8d402841555db158d9ea1c
mbelmontes7 <you@example.com>
2024-03-05 13:45:41 -0600

    Search algo

---
+++ Searches.java
@@ -0,0 +1,22 @@
+import java.util.Arrays;
+
+public class Searches {
+    public static void main(String[] args) {
+        // create an array
+        double[] payments = {3.35, 12.07, 122.76, 7.33, 6.04, 681.78, -0.05, 45.25, 107.33, 6.25, 3.45, 3.45, 0.52};
+
+        // what are we looking for?
+        double searchKey = 7.33;
+        System.out.println("Search Key found at: " + sequentialSearch(payments, searchKey));
+    }
+
+    public static int sequentialSearch(double[] arr, double key) {
+        int arraySize = arr.length;
+        for (int i = 0; i < arraySize; i++) {
+            if (arr[i] == key) {
+                return i;
+            }
+        }
+        return -1;
+    }
+}
